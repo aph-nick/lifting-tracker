3527b541669810a5095d4860d8b5b125a4feb2c7
nick <wk@kucharski.ovh>
2025-09-30 13:01:26 +0200

    Added UserMapper

--- src/main/java/com/pm/liftingtracker/mapper/user/UserMapper.java
+++ src/main/java/com/pm/liftingtracker/mapper/user/UserMapper.java
@@ -2,16 +2,39 @@ package com.pm.liftingtracker.mapper.user;
 
 import com.pm.liftingtracker.dto.user.UserResponseDTO;
 import com.pm.liftingtracker.mapper.Mapper;
+import com.pm.liftingtracker.mapper.workout.WorkoutMapper;
 import com.pm.liftingtracker.model.user.User;
 
 public class UserMapper implements Mapper<UserResponseDTO, User> {
+
+    private final WorkoutMapper workoutMapper;
+
+    public UserMapper(WorkoutMapper workoutMapper) {
+        this.workoutMapper = workoutMapper;
+    }
+
     @Override
-    public UserResponseDTO toDto(User model) {
-        return null;
+    public UserResponseDTO toDto(User user) {
+        UserResponseDTO dto = new UserResponseDTO();
+
+        dto.setId(user.getId());
+        dto.setUsername(user.getUsername());
+        dto.setWorkouts(user.getWorkouts().stream()
+                .map(workoutMapper::toDto)
+                .toList());
+
+        return dto;
     }
 
     @Override
     public User toModel(UserResponseDTO dto) {
-        return null;
+        User user = new User();
+
+        user.setUsername(dto.getUsername());
+        user.setWorkouts(dto.getWorkouts().stream()
+                .map(workoutMapper::toModel)
+                .toList());
+
+        return user;
     }
 }
