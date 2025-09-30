e4d50a554d826cfaf07bb919a8acbcaab536aac4
nick <wk@kucharski.ovh>
2025-09-30 13:01:16 +0200

    Added WorkoutMapper

--- src/main/java/com/pm/liftingtracker/mapper/workout/WorkoutMapper.java
+++ src/main/java/com/pm/liftingtracker/mapper/workout/WorkoutMapper.java
@@ -5,13 +5,34 @@ import com.pm.liftingtracker.mapper.Mapper;
 import com.pm.liftingtracker.model.workout.Workout;
 
 public class WorkoutMapper implements Mapper<WorkoutResponseDTO, Workout> {
+
+    private final ExerciseMapper exerciseMapper;
+
+    public WorkoutMapper(ExerciseMapper exerciseMapper) {
+        this.exerciseMapper = exerciseMapper;
+    }
+
     @Override
     public WorkoutResponseDTO toDto(Workout workout) {
-        return null;
+        WorkoutResponseDTO dto = new WorkoutResponseDTO();
+
+        dto.setName(workout.getName());
+        dto.setExercises(workout.getExercises().stream()
+                .map(exerciseMapper::toDto)
+                .toList());
+
+        return dto;
     }
 
     @Override
     public Workout toModel(WorkoutResponseDTO workoutResponseDTO) {
-        return null;
+        Workout workout = new Workout();
+
+        workout.setName(workoutResponseDTO.getName());
+        workout.setExercises(workoutResponseDTO.getExercises().stream()
+                .map(exerciseMapper::toModel)
+                .toList());
+
+        return workout;
     }
 }
