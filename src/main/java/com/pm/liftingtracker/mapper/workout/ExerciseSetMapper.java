6fe7f3666690256ae6d5baa5b0caf5a3f1d66506
nick <wk@kucharski.ovh>
2025-09-30 13:01:01 +0200

    Added ExerciseSetmapper

--- src/main/java/com/pm/liftingtracker/mapper/workout/ExerciseSetMapper.java
+++ src/main/java/com/pm/liftingtracker/mapper/workout/ExerciseSetMapper.java
@@ -5,13 +5,33 @@ import com.pm.liftingtracker.mapper.Mapper;
 import com.pm.liftingtracker.model.workout.ExerciseSet;
 
 public class ExerciseSetMapper implements Mapper<ExerciseSetResponseDTO, ExerciseSet> {
+
+    private final ExerciseMapper exerciseMapper;
+
+    public ExerciseSetMapper(ExerciseMapper exerciseMapper) {
+        this.exerciseMapper = exerciseMapper;
+    }
+
     @Override
     public ExerciseSetResponseDTO toDto(ExerciseSet exerciseSet) {
-        return null;
+        ExerciseSetResponseDTO dto = new ExerciseSetResponseDTO();
+
+        dto.setId(exerciseSet.getId());
+        dto.setLoad(exerciseSet.getLoad());
+        dto.setRepetitions(exerciseSet.getRepetitions());
+        dto.setExercise(exerciseMapper.toDto(exerciseSet.getExercise()));
+
+        return dto;
     }
 
     @Override
     public ExerciseSet toModel(ExerciseSetResponseDTO exerciseSetResponseDTO) {
-        return null;
+        ExerciseSet exerciseSet = new ExerciseSet();
+
+        exerciseSet.setLoad(exerciseSetResponseDTO.getLoad());
+        exerciseSet.setRepetitions(exerciseSetResponseDTO.getRepetitions());
+        exerciseSet.setExercise(exerciseMapper.toModel(exerciseSetResponseDTO.getExercise()));
+
+        return exerciseSet;
     }
 }
