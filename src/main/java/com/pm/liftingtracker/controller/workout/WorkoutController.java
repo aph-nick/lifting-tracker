f740a40775d6218ff4dc7069b7f97f7163c1e8ae
nick <wk@kucharski.ovh>
2025-10-02 20:52:21 +0200

    WorkoutController Work and Initial commit

---
+++ src/main/java/com/pm/liftingtracker/controller/workout/WorkoutController.java
@@ -0,0 +1,10 @@
+package com.pm.liftingtracker.controller.workout;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/workouts")
+public class WorkoutController {
+
+}
