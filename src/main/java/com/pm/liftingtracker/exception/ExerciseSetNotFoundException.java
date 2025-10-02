dddad29c8091b2b0f4b38e1889d15557e48ab8f6
nick <wk@kucharski.ovh>
2025-10-02 12:49:52 +0200

    added ExerciseSetNotFoundException

---
+++ src/main/java/com/pm/liftingtracker/exception/ExerciseSetNotFoundException.java
@@ -0,0 +1,7 @@
+package com.pm.liftingtracker.exception;
+
+public class ExerciseSetNotFoundException extends RuntimeException {
+    public ExerciseSetNotFoundException(String message) {
+        super(message);
+    }
+}
